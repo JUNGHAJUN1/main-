07e73ed8b3a49eb9ee7a07258b9ed919652160fd
DESKTOP-27NVL43\user <fnceric@naver.com>
2021-07-21 14:17:15 +0900

    중간

--- new.py
+++ new.py
@@ -1,5 +1,6 @@
-print("hello world")
-animal = "고양이"
+#print("hello world")
+
+'''animal = "고양이"
 name = "해피"
 age = 4
 hobby = "낮잠"
@@ -9,4 +10,71 @@ print("우리집" + animal +"의 이름은 " + name + "에요")
 hobby="공놀이"
 #print(name+"는"+str(age)+"살이며, "+hobby+"을 아주 좋아해요")
 print(name, "는 ", age, "살이며, ", hobby,"을 아주 좋아해요")
-print(name+"는 어른일까요? "+str(is_adult))
+print(name+"는 어른일까요? "+str(is_adult))'''
+
+'''print(abs(-5))
+print(pow(4,2))
+print(max(5,12))
+print(min(5,12))
+print(round(3.14))
+print(round(4.99))
+
+
+from math import*
+print(floor(4.99))
+print(ceil(3.14))
+print(sqrt(16))'''
+
+'''
+from random import*
+print(random())
+print(random()*10)
+print(int (random()*10))
+print(int (random()*10)+1)'''
+
+'''print(randrange(1,46)) 
+print(randrange(1,46)) 
+print(randrange(1,46)) 
+print(randrange(1,46)) 
+print(randrange(1,46)) 
+print(randrange(1,46))'''
+
+'''print(randint(1,45))
+print(randint(1,45))
+print(randint(1,45))
+print(randint(1,45))
+print(randint(1,45))'''
+
+'''day=randint(4,28)
+
+print("오프라인 스터디 모임 날짜는 매월",str(day),"일로 선정 되었습니다.")
+
+day1=randrange(4,29)
+
+print("오프라인 스터디 모임 날짜는 매월",str(day1),"일로 선정 되었습니다.")'''
+
+'''sentence='나는 소년입니다'
+print(sentence)
+sentence2="파이썬은 쉬워요"
+print(sentence2)
+sentence3="""나는 소년이고,
+파이썬은 쉬워요"""
+print(sentence3)
+'''
+
+'''jumin="990120-1234567"
+
+print("성별 : " + jumin[7])
+print("연 : " + jumin[0:2])
+print("월 : " + jumin[2:4])
+print("일 : " + jumin[4:6])
+
+print("생년월일 : " + jumin[:6])
+print("뒤 7자리 : " + jumin[7:])
+print("뒤 7자리 (뒤에서부터) : " + jumin[-7:])'''
+
+print(python.lowe())
+
+
+
+
